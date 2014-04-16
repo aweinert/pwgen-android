2abacc676d815c8a6f5eb8d1846d04270dd307d1
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2014-04-15 23:22:29 -0700

    Populate spinners with option choices on start

--- src/com/alexweinert/pwgen/MainActivity.java
+++ src/com/alexweinert/pwgen/MainActivity.java
@@ -1,8 +1,12 @@
 package com.alexweinert.pwgen;
 
-import android.os.Bundle;
+import java.util.LinkedList;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -16,7 +20,31 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        this.populateOptionSpinners();
+
         return true;
     }
 
+    private void populateOptionSpinners() {
+        // Taken from http://developer.android.com/guide/topics/ui/controls/spinner.html
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.optionChoices,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Use the same adapter for all spinners
+        for (Spinner spinner : this.getOptionSpinners()) {
+            spinner.setAdapter(adapter);
+        }
+    }
+
+    private Iterable<Spinner> getOptionSpinners() {
+        LinkedList<Spinner> returnValue = new LinkedList<Spinner>();
+        returnValue.add((Spinner) this.findViewById(R.id.lowercaseSpinner));
+        returnValue.add((Spinner) this.findViewById(R.id.uppercaseSpinner));
+        returnValue.add((Spinner) this.findViewById(R.id.digitsSpinner));
+        returnValue.add((Spinner) this.findViewById(R.id.symbolsSpinner));
+        returnValue.add((Spinner) this.findViewById(R.id.ambiguousSpinner));
+        return returnValue;
+    }
 }
